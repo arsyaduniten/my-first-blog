6289ce9d5c072067c86ec33efdb18fefa2890814
shads <azad@shopprapp.com>
2017-02-22 15:17:44 +0800

    Created model,view and template for post details

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns=[
 	url(r'^$', views.post_list, name='post_list'),	#assigning a view called post_list to the URL
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail') #^post/ means the URL shall begin with post and a "/".
+
+	#(?P<pk>\d+) means that django will take everything you put after the "post/" into a variable called pk as we declared on 
+	# post_list.html. "\d" means it must be a digit and "+" means it shall be one or more digit. not-valid = azad/blog/post//
+	#valid = azad/blog/post/123/ . /$ means it must end with a '/'. $ means end, ^ means begin
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post #the dot before models means the current directory or current application
 
@@ -7,4 +7,9 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts}) #the stuff inside {} is for the template to use.
 
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post})
+
+	
 
